505cd03cfa687112ebc58b3ba421d91c0b127d42
chid-aakash <aakashchid99@gmail.com>
2025-07-05 09:11:10 +0530

    satandard packages

--- crm/sentra/doctype/standard_package/standard_package.py
+++ crm/sentra/doctype/standard_package/standard_package.py
@@ -7,3 +7,54 @@ from frappe.model.document import Document
 
 class StandardPackage(Document):
 	pass
+
+	@staticmethod
+	def default_list_data():
+		"""
+		Provide default list view configuration for Standard Package doctype so that
+		the generic get_data helper can build column and row definitions without
+		running into AttributeError.
+		"""
+		columns = [
+			{
+				"label": "Package Name",
+				"type": "Data",
+				"key": "package_name",
+				"width": "16rem",
+			},
+			{
+				"label": "No. of Days",
+				"type": "Int",
+				"key": "no_of_days",
+				"width": "8rem",
+			},
+			{
+				"label": "No. of Nights",
+				"type": "Int",
+				"key": "no_of_nights",
+				"width": "8rem",
+			},
+			{
+				"label": "Price",
+				"type": "Currency",
+				"key": "price",
+				"width": "10rem",
+			},
+			{
+				"label": "Last Modified",
+				"type": "Datetime",
+				"key": "modified",
+				"width": "10rem",
+			},
+		]
+
+		rows = [
+			"name",
+			"package_name",
+			"no_of_days",
+			"no_of_nights",
+			"price",
+			"modified",
+		]
+
+		return {"columns": columns, "rows": rows}
